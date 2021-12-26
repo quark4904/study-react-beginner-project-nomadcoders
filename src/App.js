86ec2eaa055d1537336c13361ece8d1563205461
Jongmin Choi <quark4904@gmail.com>
2021-12-27 00:22:18 +0900

    Add: #5.1 Tour of CRA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className={styles.title}>Hello, World!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
