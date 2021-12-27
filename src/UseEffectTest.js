6f2e900a81ba8f5283849444c7ae6fd391798485
Jongmin Choi <quark4904@gmail.com>
2021-12-28 00:34:42 +0900

    Add: #6.2 Deps

--- src/UseEffectTest.js
+++ src/UseEffectTest.js
@@ -2,15 +2,31 @@ import { useState, useEffect } from "react";
 
 function UseEffectTest() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => {
     setCounter((event) => event + 1);
   };
-  console.log("call an api");
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => console.log("I run only once"), []);
-
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'counter' & 'keyword' change");
+  }, [counter, keyword]);
   return (
     <div>
+      <input
+        type="text"
+        placeholder="search..."
+        onChange={onChange}
+        value={keyword}
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
